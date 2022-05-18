fb406ab6a239378d452d3abee54765a3e71d636b
Yunus Can Kuyumcu <65031891+cnkymc@users.noreply.github.com>
2022-05-18 18:35:10 +0300

    Update index.js

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,3 +1,3 @@
 
-export const API_URL='https://api.collectapi.com/corona/'
-export const API_TOKEN='apikey 4x5Y1EbILDs96m79dj3i0T:7D9ziudsDDUMmPNZdPRBPS'
+export const API_URL='YOUR API'
+export const API_TOKEN='YOUR TOKEN'
